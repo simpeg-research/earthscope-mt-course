a31683f3bdda3c56f6dfaa058526a24fdc10fbe2
JP <peacock.jared@gmail.com>
2022-10-17 16:55:07 -0600

    Adding .bat file

---
+++ mt_shortcourse_install.bat
@@ -0,0 +1,27 @@
+git clone https://github.com/simpeg/iris-mt-course-2022.git
+cd iris-mt-course-2022
+conda env create -f environment.yml
+conda activate em
+git clone https://github.com/kujaku11/mt_metadata.git
+cd mt_metadata
+python setup.py install
+cd ..
+git clone https://github.com/kujaku11/mth5.git
+cd mth5
+python setup.py install
+cd ..
+git clone https://github.com/MTgeophysics/mtpy.git
+git checkout -f v2
+cd mtpy
+python setup.py install
+cd ..
+git clone https://github.com/simpeg/aurora.git
+cd aurora
+pip install -e .
+cd ..
+git clone https://github.com/simpeg/simpeg.git
+cd simpeg
+pip install -e .
+cd ..
+cd iris-mt-course-2022
+jupyter lab
